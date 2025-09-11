12dd4dde3a335c735c3ac83443751369c8287db5
howsetya <howsetya@googlemail.com>
2025-09-11 18:00:02 +0200

    Before merging with main

--- cmds/cmdJoin.cpp
+++ cmds/cmdJoin.cpp
@@ -1,54 +1,50 @@
-#include "cmds_list.hpp"
+#include "../lib_irc.hpp"
 
-int cmdJoin(RawTextLine msg, Client cinfo)
+void	cmdJoin(Server &server, RawTextLine &line, Client &client)
 {
-    // Check if we have any parameters
-    const std::vector<std::string>& params = msg.getParams();
-    if (params.empty())
-    {
-        // ERR_NEEDMOREPARAMS (461)
-        std::string error = ":localhost 461 " + cinfo.getNickname() + " JOIN :Not enough parameters\r\n";
-        send(cinfo.getFd(), error.c_str(), error.length(), 0);
-        return 1;
-    }
+	if (checkParams(line, client) == 1)
+		return ;
+	std::vector<std::string> channels = line.getSepParams();
+	for (std::vector<std::string>::const_iterator it = channels.begin(); it != channels.end(); ++it)
+	{
+		std::string channelName = *it;
+		if (channelName[0] != '#')
+			channelName = "#" + channelName;
+		Channel* channel = new Channel(channelName);
+		
+		// Add user to channel (this will need to be implemented in Channel class)
+		// channel->addUser(client);
+		// Send JOIN confirmation to the channel
+		std::string joinline = ":" + client.getNickname() + "!" + client.getUsername() + "@" + 
+							client.getIP() + " JOIN " + channelName + "\r\n";
+		
+		// Send to all users in channel (this will need to be implemented in Channel class)
+		channel->broadcast(joinline);
+		
+		// Send channel topic if it exists
+		std::string topic = channel->getTopic();
+		if (!topic.empty())
+		{
+			std::string topicline = ":localhost 332 " + client.getNickname() + " " + 
+								channelName + " :" + topic + "\r\n";
+			send(client.getFd(), topicline.c_str(), topicline.length(), 0);
+		}
+		
+		// TODO: Send names list (353) and end of names (366)
+		// This requires implementation of getting user list from channel
+	}
+	
+	return;
+}
 
-    // First parameter contains comma-separated channel list
-    std::vector<std::string> channels = msg.getSepParams();
-    
-    // Check each channel name
-    for (std::vector<std::string>::const_iterator it = channels.begin(); it != channels.end(); ++it)
-    {
-        std::string channelName = *it;
-        
-        // Add # if not present
-        if (channelName[0] != '#')
-            channelName = "#" + channelName;
-
-        // Create or get existing channel
-        Channel* channel = new Channel(channelName);  // You'll need channel management in your Server class
-        
-        // Add user to channel (this will need to be implemented in Channel class)
-        // channel->addUser(cinfo);
-        
-        // Send JOIN confirmation to the channel
-        std::string joinMsg = ":" + cinfo.getNickname() + "!" + cinfo.getUsername() + "@" + 
-                            cinfo.getIP() + " JOIN " + channelName + "\r\n";
-        
-        // Send to all users in channel (this will need to be implemented in Channel class)
-        channel->broadcast(joinMsg);
-        
-        // Send channel topic if it exists
-        std::string topic = channel->getTopic();
-        if (!topic.empty())
-        {
-            std::string topicMsg = ":localhost 332 " + cinfo.getNickname() + " " + 
-                                channelName + " :" + topic + "\r\n";
-            send(cinfo.getFd(), topicMsg.c_str(), topicMsg.length(), 0);
-        }
-        
-        // TODO: Send names list (353) and end of names (366)
-        // This requires implementation of getting user list from channel
-    }
-    
-    return 0;
+int	checkParams(RawTextLine &line, Client &client)
+{
+	const std::vector<std::string>& params = line.getParams();
+	if (params.empty())
+	{
+		std::string error = ":localhost 461 " + client.getNickname() + " JOIN :Not enough parameters\r\n";
+		send(client.getFd(), error.c_str(), error.length(), 0);
+		return 1;
+	}
+	return 0;
 }
--- cmds/cmdPrivmsg.cpp
+++ cmds/cmdPrivmsg.cpp
@@ -1,16 +1,21 @@
-#include "lib_irc.hpp"
+#include "../lib_irc.hpp"
 
-void cmdPrivmsg(Server &server ,RawTextLine &line, Client &client)
+void cmdPrivmsg(Server &server, RawTextLine &line, Client &client)
 {
-    // Check if the destination is User, Channel
+    std::vector<Client*> client_list;
+    server.check_client(line, client_list);
 
-    // search_user() Look up the user in the server's client list;
-        // send message to client
+    if (client_list.empty())
+        return;
 
-        // else error
-    
-    // search_channel() Look up the user in the server's channel list;
-        // send message to channel
+    for (size_t i = 0; i < client_list.size(); ++i)
+    {
+        Client* target = client_list[i];
+        std::string msg = std::string(":") + client.getNickname() + " PRIVMSG " + target->getUsername() + " :" + line.getTrailing() + "\r\n";
+        std::cout << msg << std::endl;
+       // std::string msg1 = std::string(":") + target->getNickname() + " PRIVMSG " + target->getUsername() + " :" + line.getTrailing() + "\r\n";
 
-        // else error
+        send(target->getFd(), msg.c_str(), msg.size(), 0);
+       // send(client.getFd(), msg1.c_str(), msg1.size(), 0);
+    }
 }
