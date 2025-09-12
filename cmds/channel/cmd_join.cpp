1074280716519cd0d1ea57e82818ae08f962ea29
howsetya <howsetya@googlemail.com>
2025-09-12 15:22:23 +0200

    Update channel_name preposition

--- cmds/channel/cmd_join.cpp
+++ cmds/channel/cmd_join.cpp
@@ -7,7 +7,7 @@ void	cmd_join(Server &server, RawTextLine &line, Client &client)
 	if (checkJoinParams(line, client) == 1)
 		return ;
 	std::string channel_name = line.getParams()[0];
-	if (channel_name[0] != '#')
+	if (channel_name[0] != '#' && channel_name[0] != '&' && channel_name[0] != '+' && channel_name[0] != '!')
 		channel_name = "#" + channel_name;
 	server.addChannel(channel_name);
 	Channel* channel = server.getChannel(channel_name);
