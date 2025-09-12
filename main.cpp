b1c8e30b4020c16e85f6f5ce8d094fa81350667c
Grace Mullin <gracemullin2010@gmail.com>
2025-09-12 15:50:37 +0200

    password and port num requirement added

--- main.cpp
+++ main.cpp
@@ -1,17 +1,28 @@
 #include "lib_irc.hpp"
 
+int	invalid_arguments(char **argv)
+{
+    if (std::string(argv[1]) != "6667")
+        return 1;
+    if (std::string(argv[2]) != "password42")
+        return 1;
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        std::cerr << "three arguments" << std::endl;
+        std::cerr << "Please use the following format:" << std::endl;
+        std::cerr << "./ircserv <port> <password>" << std::endl;
         return 1;
     }
-    
+    if (invalid_arguments(argv))
+    {
+        std::cout << "Incorrect port number or password entered. Please try again." << std::endl;
+        return 0;
+    }
     Server srv;
-
     srv.srv_run();
-
-    (void)argv;
-    
+    return 0;
 }
