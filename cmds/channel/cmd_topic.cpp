8fc6d68ca17e27c45a3a641b7f4944743c7700ec
howsetya <howsetya@googlemail.com>
2025-09-16 12:57:43 +0200

    TOPIC: looks ok but dont know whats wrong

--- cmds/channel/cmd_topic.cpp
+++ cmds/channel/cmd_topic.cpp
@@ -3,40 +3,24 @@
 void	cmd_topic(Server &server, RawTextLine &line, Client &client);
 int		check_topic_params(RawTextLine &line, Client &client, std::string server_name);
 int		check_topic_channel(Server &server, Client &client, std::string channel_name, std::string server_name);
-void	topic_setquery();
+void	topic_queryandset(Server &server, Client &client, const std::vector<std::string> &params, std::string channel_name , std::string server_name);
 
-#define RES "\033[0m"
-#define RED "\033[31m"
+// #define RES "\033[0m"
+// #define RED "\033[31m"
 
 void	cmd_topic(Server &server, RawTextLine &line, Client &client)
 {
-	(void)server;
-	//(void)line;
-	//(void)(client);
 	char server_name[256];
 	if (gethostname(server_name, sizeof(server_name)) != 0)
 		strcpy(server_name, "localhost");
 	if (check_topic_params(line, client, server_name) == 1)
 		return;
-	std::cout << RED << "works fine" << RES << std::endl;
+	//std::cout << RED << "works fine" << RES << std::endl;
 	const std::vector<std::string> &params = line.get_sep_params();
 	std::string channel_name = params[0];
-	if (check_topic_channel(server, client, channel_name, server_name) == 1);
+	if (check_topic_channel(server, client, channel_name, server_name) == 1)
 		return;
-	
-	// std::string channel_topic = params[1];
-	// Channel *channel = server.get_channel(channel_name);
-	// std::cout << RED << "Channel name: " << channel_name << RES << std::endl;
-	// std::cout << RED << "Channel: " << channel << RES << std::endl;
-	//if (params[1])
-
-	// if (params.size() == 1)
-	// {
-	// 	std::string print_topic = channel->get_topic() + "\r\n";
-	// 	send(client.get_FD(), print_topic.c_str(), print_topic.length(), 0);
-	// }
-	// else
-	// 	channel->set_topic(params[1]);
+	topic_queryandset(server, client, params, channel_name, server_name);
 }
 
 int	check_topic_params(RawTextLine &line, Client &client, std::string server_name)
@@ -69,4 +53,41 @@ int	check_topic_channel(Server &server, Client &client, std::string channel_name
 		return 1;
 	}
 	return 0;
+}
+
+void	topic_queryandset(Server &server, Client &client, const std::vector<std::string> &params, std::string channel_name , std::string server_name)
+{
+	Channel *channel = server.get_channel(channel_name);
+	if (params.size() == 1) 
+	{
+		std::string topic = channel->get_topic();
+		if (topic.empty())
+		{
+			std::string rpl_notopic = ":" + std::string(server_name) + " 331 " + 
+					client.get_nickname() + " " + channel_name + " :No topic is set\r\n";
+			send(client.get_FD(), rpl_notopic.c_str(), rpl_notopic.size(), 0);
+		}
+		else
+		{
+			std::string rpl_topic = ":" + std::string(server_name) + " 332 " + 
+					client.get_nickname() + " " + channel_name + " :" + topic + "\r\n";
+			send(client.get_FD(), rpl_topic.c_str(), rpl_topic.size(), 0);
+		}
+	} 
+	else
+	{
+		std::string new_topic = params[1];
+		// if (channel->is_mode_t() && !channel->is_operator(client.get_FD()))
+		// {
+		// 	std::string err = ":localhost 482 " + client.get_nickname() + " " + channel_name + " :You're not channel operator\r\n";
+		// 	send(client.get_FD(), err.c_str(), err.size(), 0);
+		// 	return;
+		// }
+		channel->set_topic(new_topic);
+		std::string prefix = ":" + client.get_nickname() + "!" + client.get_username() + "@" + client.get_hostname();
+		std::string announce = prefix + " TOPIC " + channel_name + " :" + new_topic + "\r\n";
+		const std::vector<Client>& users = channel->get_users();
+			for (size_t i = 0; i < users.size(); ++i)
+			send(users[i].get_FD(), announce.c_str(), announce.length(), 0);
+	}
 }
