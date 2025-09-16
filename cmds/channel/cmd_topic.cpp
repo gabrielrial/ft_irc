131edef9af97235d49ed794cfe8215bee7dd7607
howsetya <howsetya@googlemail.com>
2025-09-16 12:29:17 +0200

    TOPIC: check channel existence and user membership in channel

--- cmds/channel/cmd_topic.cpp
+++ cmds/channel/cmd_topic.cpp
@@ -1,9 +1,9 @@
 #include "../../lib_irc.hpp"
 
 void	cmd_topic(Server &server, RawTextLine &line, Client &client);
-int		check_topic_params(RawTextLine &line, Client &client, char *server_name);
-int		check_topic_channel();
-void	parse_topicmessage();
+int		check_topic_params(RawTextLine &line, Client &client, std::string server_name);
+int		check_topic_channel(Server &server, Client &client, std::string channel_name, std::string server_name);
+void	topic_setquery();
 
 #define RES "\033[0m"
 #define RED "\033[31m"
@@ -19,8 +19,11 @@ void	cmd_topic(Server &server, RawTextLine &line, Client &client)
 	if (check_topic_params(line, client, server_name) == 1)
 		return;
 	std::cout << RED << "works fine" << RES << std::endl;
-	//const std::vector<std::string> &params = line.get_sep_params();
-	//std::string channel_name = params[0];
+	const std::vector<std::string> &params = line.get_sep_params();
+	std::string channel_name = params[0];
+	if (check_topic_channel(server, client, channel_name, server_name) == 1);
+		return;
+	
 	// std::string channel_topic = params[1];
 	// Channel *channel = server.get_channel(channel_name);
 	// std::cout << RED << "Channel name: " << channel_name << RES << std::endl;
@@ -36,7 +39,7 @@ void	cmd_topic(Server &server, RawTextLine &line, Client &client)
 	// 	channel->set_topic(params[1]);
 }
 
-int	check_topic_params(RawTextLine &line, Client &client, char *server_name)
+int	check_topic_params(RawTextLine &line, Client &client, std::string server_name)
 {
 	if (line.get_params().empty())
 	{
@@ -48,4 +51,22 @@ int	check_topic_params(RawTextLine &line, Client &client, char *server_name)
 	return 0;
 }
 
-int		check_topic_channel(Server &server, Client &client, std::string channel_name);
+int	check_topic_channel(Server &server, Client &client, std::string channel_name, std::string server_name)
+{
+	Channel *channel = server.get_channel(channel_name);
+	if (!channel)
+	{
+		std::string err_nosuchchannel = ":" + std::string(server_name) + " 403 " + 
+				client.get_nickname() + " " + channel_name + " :No such channel\r\n";
+		send(client.get_FD(), err_nosuchchannel.c_str(), err_nosuchchannel.size(), 0);
+		return 1;
+	}
+	else if (!channel->has_user(client))
+	{
+		std::string err_notonchannel = ":" + std::string(server_name) + " 442 " + 
+				client.get_nickname() + " " + channel_name + " :You're not on that channel\r\n";
+		send(client.get_FD(), err_notonchannel.c_str(), err_notonchannel.size(), 0);
+		return 1;
+	}
+	return 0;
+}
