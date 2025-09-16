655589b61b6fccee34bc49942a5d693abfdd1967
gabrielrial <gabrielrialpicco@gmail.com>
2025-09-16 16:45:53 +0200

    cmd_who() added

--- cmds/user_info/cmd_who.cpp
+++ cmds/user_info/cmd_who.cpp
@@ -6,7 +6,7 @@
 /*   By: grial <grial@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 11:13:06 by grial             #+#    #+#             */
-/*   Updated: 2025/09/16 13:58:02 by grial            ###   ########.fr       */
+/*   Updated: 2025/09/16 16:43:38 by grial            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,44 @@
 
 void cmd_who(Server &server, RawTextLine &line, Client &client)
 {
-	//if (line.getParams().empty())
-	//{
-	//	std::cout << "Return an error cmd_who()" << std::endl;
-	//	return;
-	//}
-	//else if (!server.check_channel(line))
-	//{
-	//	std::cout << "Channel does not exist cmd_who()" << std::endl;
-	//	return;
-	//}
-	//for (size_t i = 0; i < server.get_vector_channels().size(); i++)
-	//{
-	//	server.get_vector_channels()[i].getUsers();
-	//}
+	if (line.get_params().empty())
+	{
+		std::cout << "Return an error cmd_who()" << std::endl;
+		return;
+	}
+	else if (!server.check_channel(line))
+	{
+		std::cout << "Channel does not exist cmd_who()" << std::endl;
+		return;
+	}
+
+	for (size_t i = 0; i < line.get_params().size(); i++)
+	{
+		std::string prefix = ":" +
+							 std::string(SERVER_NAME) + " 332 " +
+							 client.get_nickname() + " * " + line.get_params()[i] + " :";
+		std::string name_list;
+		for (unsigned int x = 0; x < server.get_channel(line.get_params()[0])->get_UserCount(); x++)
+		{
+			std::string name = server.get_vector_channels()[0].get_users()[x].get_nickname();
+			if (std::string(prefix + name_list + name + " ").length() > BUFFER_LINE)
+			{
+				send(client.get_FD(), (prefix + name_list + CRLF).c_str(), (prefix + name_list + CRLF).length(), 0);
+				name_list.clear();
+				name_list = prefix + name;
+			}
+			else if (name_list.empty())
+				name_list = name;	
+			else
+				name_list = name_list + " " + name;
+		}
 	
+		send(client.get_FD(), (prefix + name_list + CRLF).c_str(), (prefix + name_list + CRLF).length(), 0);
+		std::cout << prefix + name_list + CRLF << std::endl;
+		//:lead.libera.chat 366 TuNick #linux :End of /NAMES list.
+		prefix = ":" + std::string(SERVER_NAME) + " 366 " + client.get_nickname() + " " + line.get_params()[i] + " :";
+		send(client.get_FD(), (prefix + "End of /WHO list." + CRLF).c_str(), (prefix + "End of /WHO list." + CRLF).length(), 0);
+		std::cout << prefix + "End of /WHO list." << std::endl;
+
+	}
 }
--- server/Server.hpp
+++ server/Server.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <netinet/in.h>   // sockaddr_in
-#include <sys/socket.h>   // socket functions
-#include <unistd.h>       // close()
-#include <string>         // std::string
-#include <vector>         // std::vector
+#include <netinet/in.h> // sockaddr_in
+#include <sys/socket.h> // socket functions
+#include <unistd.h>		// close()
+#include <string>		// std::string
+#include <vector>		// std::vector
 
 #include "../client/Client.hpp"
 #include "../channel/Channel.hpp"
@@ -29,30 +29,29 @@ private:
 	std::vector<int> _fdsToClose;
 
 	void register_client();
-	Client* find_client(int fd);
+	Client *find_client(int fd);
 
 	void process_line(int fd, const std::string &line);
 	void handle_client_data(int fd, char *buffer, ssize_t bytes_read, std::string &lineBuffer);
 
-	
-	public:
-		Server(uint16_t port, std::string password);
-		~Server();
-	
-		void check_client(RawTextLine &line, std::vector<Client*> &client_list);
-		bool check_channel(RawTextLine &line);
-		void	welcome(Client client);
+public:
+	Server(uint16_t port, std::string password);
+	~Server();
 
-		void		add_channel(const std::string &ch_name);
-		Channel*	get_channel(const std::string &ch_name);
+	void check_client(RawTextLine &line, std::vector<Client *> &client_list);
+	bool check_channel(RawTextLine &line);
+	void welcome(Client client);
 
-		const std::vector<Channel>& get_vector_channels() const;
+	void add_channel(const std::string &ch_name);
+	Channel *get_channel(const std::string &ch_name);
 
-		std::string get_password() const;
-		void 		remove_closed_clients(std::string lineBuffer[]);
-		void		debug_print_chan() const;
-		void		debug_print_chan_users(const Channel& chan) const;
+	const std::vector<Channel> &get_vector_channels() const;
 
-		void srv_run();
-		void schedule_close(int fd);
+	std::string get_password() const;
+	void remove_closed_clients(std::string lineBuffer[]);
+	void debug_print_chan() const;
+	void debug_print_chan_users(const Channel &chan) const;
+
+	void srv_run();
+	void schedule_close(int fd);
 };
