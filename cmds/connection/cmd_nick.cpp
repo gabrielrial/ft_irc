cc40fee09ce07d53c97454b6f309ee6c3215afdc
Grace Mullin <gracemullin2010@gmail.com>
2025-09-16 17:10:19 +0200

    nick command improved but need to add broadcast msg still

--- cmds/connection/cmd_nick.cpp
+++ cmds/connection/cmd_nick.cpp
@@ -23,13 +23,14 @@ void cmd_nick(Server &server, RawTextLine &line, Client &client)
 		send(client.get_FD(), msg.c_str(), msg.size(), 0);
 		return ;
 	}
-	if (client.has_nick()) // if 'correctly'/'successfully' changing nickname
+	if (client.has_nick())
 	{
 		const std::string oldNick = client.get_nickname();
-		client.set_nickname(newNick);
 		std::string nickMsg = ":" + oldNick + "!" + client.get_username() + "@" + client.get_hostname()
 			+ " NICK :" + newNick + "\r\n";
 		const std::string msg = "You are now known as " + line.get_sep_params()[0] + "\r\n";
 		send(client.get_FD(), msg.c_str(), msg.size(), 0);
+		
 	}
+	client.set_nickname(newNick);
 }
