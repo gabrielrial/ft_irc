da2af52398935598147d7505c40b971382884bda
gabrielrial <gabrielrialpicco@gmail.com>
2025-09-20 19:27:21 +0200

    privmsg and notice working

---
+++ cmds/messaging/messaging_utils.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include "../../lib_irc.hpp"
+
+void send_message_to_channel(Channel *channel, Client &sender,
+							 const std::string &targetName, const std::string &message,
+							 const std::string &command, bool sendErrors);
+
+void send_message_to_user(Server &server, Client &sender,
+						  const std::string &targetName, const std::string &message,
+						  const std::string &command, bool sendErrors);
+
+void send_message(Server &server, Client &sender,
+				  const std::vector<std::string> &targets, const std::string &message,
+				  const std::string &command, bool sendErrors);
+
+std::vector<std::string> get_target(RawTextLine &line);
