38e72b5b37ca39c4efea2b81e717fc3600667cdc
howsetya <howsetya@googlemail.com>
2025-09-29 14:32:02 +0200

    Better format for dcct module 3

--- dcc/DCCTransfer.hpp
+++ dcc/DCCTransfer.hpp
@@ -5,16 +5,16 @@
 class DCCTransfer
 {
 private:
-	std::string _sender;
-	std::string _receiver;
-	std::string _filename;
-	size_t _filesize;
-	std::string _ip;
-	int _socket;
-	bool _is_sender;
-	std::time_t _start_time;
-	size_t _bytes_transferred;
-	int _file_fd;
+	std::string	_sender;
+	std::string	_receiver;
+	std::string	_filename;
+	size_t		_filesize;
+	std::string	_ip;
+	int			_socket;
+	bool		_is_sender;
+	std::time_t	_start_time;
+	size_t		_bytes_transferred;
+	int			_file_fd;
 
 public:
 	DCCTransfer(const std::string &sender, const std::string &receiver,
@@ -22,22 +22,22 @@ public:
 	~DCCTransfer();
 
 	// Getter methods
-	const std::string &get_sender() const;
-	const std::string &get_receiver() const;
-	const std::string &get_filename() const;
-	size_t get_filesize() const;
-	int get_socket() const;
-	bool is_sender() const;
-	time_t get_start_time() const;
-	size_t get_bytes_transferred() const;
+	const std::string	&get_sender() const;
+	const std::string	&get_receiver() const;
+	const std::string	&get_filename() const;
+	size_t				get_filesize() const;
+	int					get_socket() const;
+	bool				is_sender() const;
+	time_t				get_start_time() const;
+	size_t				get_bytes_transferred() const;
 
 	// Setter methods
-	void set_socket(int socket);
-	void set_is_sender(bool is_sender);
-	void set_port(int port);
-	void add_bytes_transferred(size_t bytes);
-	bool is_complete() const;
+	void				set_socket(int socket);
+	void				set_is_sender(bool is_sender);
+	//void				set_port(int port);
+	void				add_bytes_transferred(size_t bytes);
+	bool				is_complete() const;
 	
 	// Additional getter
-	int get_port() const;
+	//int get_port() const;
 };
