d8e468cc805c9763c1c0ebbb0a1437b708de23a4
gabrielrial <gabrielrialpicco@gmail.com>
2026-06-25 02:33:08 +0200

    chore: remove tracked shell scripts from repository

--- netcat_script1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-{
-    echo -e "PASS 42"
-    echo -e "USER canbeanything * 0 :netcattop"
-    echo -e "NICK nct"
-    cat
-} | nc localhost 6667
--- netcat_script2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-{
-    echo -e "PASS 42"
-    echo -e "USER nc0!@:%m cluster_host_name () :netcatbottom"
-    echo -e "NICK ncb"
-    cat
-} | nc localhost 6667
